6f0b724907b92ebec464318a97d93221543aec05
Chris Robinson <chris.kcat@gmail.com>
2015-07-12 14:48:22 -0700

    Load the map names and table info

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -4,6 +4,10 @@
 #include <osgViewer/Viewer>
 #include <osg/Light>
 
+#include "components/vfs/manager.hpp"
+
+#include "log.hpp"
+
 
 namespace DF
 {
@@ -22,6 +26,57 @@ World::~World()
 
 void World::initialize(osgViewer::Viewer *viewer)
 {
+    std::set<std::string> names = VFS::Manager::get().list("MAPNAMES.[0-9]*");
+    if(names.empty()) throw std::runtime_error("Failed to find any regions");
+
+    for(const std::string &name : names)
+    {
+        size_t pos = name.rfind('.');
+        if(pos == std::string::npos)
+            continue;
+        std::string regstr = name.substr(pos+1);
+        unsigned long regnum = std::stoul(regstr, nullptr, 10);
+
+        VFS::IStreamPtr stream = VFS::Manager::get().open(name.c_str());
+        if(!stream) throw std::runtime_error("Failed to open "+name);
+
+        uint32_t mapcount = VFS::read_le32(*stream);
+        if(mapcount == 0) continue;
+
+        MapRegion region;
+        region.mNames.resize(mapcount);
+        for(std::string &mapname : region.mNames)
+        {
+            char mname[32];
+            if(!stream->read(mname, sizeof(mname)) || stream->gcount() != sizeof(mname))
+                throw std::runtime_error("Failed to read map names from "+name);
+            mapname.assign(mname, sizeof(mname));
+            size_t end = mapname.find('\0');
+            if(end != std::string::npos)
+                mapname.resize(end);
+        }
+        stream = nullptr;
+
+        std::string fname = "MAPTABLE."+regstr;
+        stream = VFS::Manager::get().open(fname.c_str());
+        if(!stream) throw std::runtime_error("Failed to open "+fname);
+
+        region.mTable.resize(region.mNames.size());
+        for(MapTable &maptable : region.mTable)
+        {
+            maptable.mMapId = VFS::read_le32(*stream);
+            maptable.mUnknown1 = stream->get();
+            maptable.mLongitudeType = VFS::read_le32(*stream);
+            maptable.mLatitude = VFS::read_le16(*stream);
+            maptable.mUnknown2 = VFS::read_le16(*stream);
+            maptable.mUnknown3 = VFS::read_le32(*stream);
+        }
+        stream = nullptr;
+
+        if(regnum >= mRegions.size()) mRegions.resize(regnum+1);
+        mRegions[regnum] = std::move(region);
+    }
+
     mViewer = viewer;
 
     mViewer->setLightingMode(osg::View::HEADLIGHT);
--- src/world/world.hpp
+++ src/world/world.hpp
@@ -3,14 +3,39 @@
 
 #include "iface.hpp"
 
+#include <vector>
+#include <string>
+
 #include <osg/ref_ptr>
 
 
 namespace DF
 {
 
+struct MapTable {
+    uint32_t mMapId;
+    uint8_t mUnknown1;
+    union {
+        uint32_t mLongitudeType;
+        struct {
+            uint32_t mLongitude : 17;
+            uint32_t mType : 15;
+        };
+    };
+    uint16_t mLatitude;
+    uint16_t mUnknown2;
+    uint32_t mUnknown3;
+};
+
+struct MapRegion {
+    std::vector<std::string> mNames;
+    std::vector<MapTable> mTable;
+};
+
 class World : public WorldIface {
     osg::ref_ptr<osgViewer::Viewer> mViewer;
+
+    std::vector<MapRegion> mRegions;
 
     World();
     ~World();
